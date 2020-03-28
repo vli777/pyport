da76e40ca392fe4afb0367dd9af2cf75fe17aa09
Vince L <10236284+vintg@users.noreply.github.com>
2020-03-28 16:25:16 -0400

    refactor: yahoo data imports using csv symbol list

--- portfolio optimization.py
+++ portfolio optimization.py
@@ -10,33 +10,55 @@ from pypfopt.efficient_frontier import EfficientFrontier
 from datetime import datetime
 import os
 import re
+import yfinance as yf
+yf.pdr_override()
+import csv
 
-## Config
+## config
 folder = '1 yr'
-weight_bounds=(-1,1)
-capital = 20000
-max_vol = .33
-target_return = .33
-viewdict = { "QQQ": 0.10, "TLT": .10 } # need views on each asset to work properly
-use_bl_views = False # uses market return derived weights
-opt = 'sharpe' # black, min vol, target vol, target return
+input_file = 'portfolio inputs.csv'
+symbols = ['TLT','IWM']
+import_csv_data = False     # if using exported yahoo finance data, set this to True
+startDate = '2018-03-27'    
+endDate = '2020-03-27'
+weight_bounds=(0,1)         # (-1, 1) to include shorts
+capital = 20000             # starting capital
+opt = 'black'              # black, min vol, target vol, target return
+
+# opt method specific vars  
+viewdict = {                # if using BL, need prior weights on each asset to work properly
+    "TLT": 1,               # these weights are based on your belief of expected performance
+    "IWM": -1 
+} 
+max_vol = .33               # if maximizing return for target vol
+target_return = .33         # if minimizing vol for a target return
 
 ## start main
 DATE = datetime.now().strftime("%Y-%m-%d_%H%M%S")
 PATH = os.getcwd() +'/' + folder +'/'
 
 # get ticker symbols
-symbols = []
-for file in os.listdir(PATH):
-    if file.endswith(".csv") and not re.search(r'\d', file):
-        name = os.path.splitext(file)[0]
-        symbols.append(name)  
+if len(symbols) == 0:
+    if import_csv_data == True:
+        for file in os.listdir(PATH):
+            print(file)
+            if file.endswith(".csv") and not re.search(r'\d', file) and file != input_file :
+                name = os.path.splitext(file)[0]
+                symbols.append(name)  
+    else:    
+        with open(PATH + input_file) as file:
+            for line in file:
+                symbols.append(line.rstrip())
 
 # Read in price data
 df = pd.DataFrame()
 
-for sym in symbols:
-    df_sym = pd.read_csv(PATH + '{}.csv'.format(sym), parse_dates=True, index_col="Date")
+for sym in symbols:    
+    if import_csv_data:
+        df_sym = pd.read_csv(PATH + '{}.csv'.format(sym), parse_dates=True, index_col="Date")
+    else:
+        df_sym = pdr.get_data_yahoo(sym, start=startDate, end=endDate)
+
     df_sym.rename(columns={'Adj Close':sym}, inplace=True)
     df_sym.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
 
@@ -49,14 +71,6 @@ mu = mean_historical_return(df)
 cov_matrix = CovarianceShrinkage(df).ledoit_wolf()
 ef = EfficientFrontier(mu, cov_matrix, weight_bounds)
 
-def getBLweights(viewdict, use_bl_views):
-    bl = BlackLittermanModel(cov_matrix, absolute_views=viewdict)
-    rets = bl.bl_returns()
-    ef = EfficientFrontier(rets, cov_matrix)
-    delta = black_litterman.market_implied_risk_aversion(df.SPY)
-    bl.bl_weights(delta)
-    return bl.clean_weights()
-
 if opt == 'sharpe':
     weights = ef.max_sharpe()
 elif opt == 'min vol':
@@ -65,8 +79,17 @@ elif opt == 'target vol':
     weights = ef.efficient_risk(max_vol)
 elif opt == 'target return':
     weights = ef.efficient_return(target_return)
-else:
-    weights = getBLweights(viewdict, use_bl_views)
+elif opt == 'black':
+    bl = BlackLittermanModel(cov_matrix, absolute_views=viewdict)    
+    spx = pdr.get_data_yahoo('SPY', start=startDate, end=endDate)
+    spx.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+    delta = black_litterman.market_implied_risk_aversion(spx['Adj Close'])
+    bl.bl_weights(delta)
+    weights = bl.clean_weights()
+    bl.portfolio_performance(verbose=True)
+
+if opt != 'black':
+    ef.portfolio_performance(verbose=True)
 
 # discrete share allocation
 latest_prices = get_latest_prices(df)
@@ -74,4 +97,3 @@ da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=capital)
 allocation, leftover = da.lp_portfolio()
 for sym in sorted(allocation):
     print(sym, + allocation[sym])
-print("Funds remaining: ${:.2f}".format(leftover))
