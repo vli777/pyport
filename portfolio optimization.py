8d4751a9898546f6cc6c3a7a875a6bfe4b8e02b3
Vince L <10236284+vintg@users.noreply.github.com>
2020-04-21 18:19:37 -0400

    refactor: rename vars, unique symbol list

--- portfolio optimization.py
+++ portfolio optimization.py
@@ -19,56 +19,45 @@ import csv
 import_symbols_from_csv = True          # if not importing, use manually entered list
 folder = '1 yr'                         # optional: if your files are located in another folder
 input_file = 'portfolio inputs.csv'     # specify the input file name with file ext
-symbols = []                            # manual symbol list
-ignored_symbols = ['GLD','ZM']          # symbols in this list will not be included in optimization  
-
-# optimization parameters
-yrs = 0.5                 # investment horizon / lookback period
-weight_bounds=(0, 1)      # e.g. (-1, 1) includes shorts
-capital = 30000           # starting capital
-opt = 'sharpe'            # sharpe, black, min portfolio vol, target vol (max return), target return (minimizes vol)
-
-# other options
+symbols = []                            
+ignored_symbols = []                    
+time_period_in_yrs = 0.5        
+weight_bounds=(0, .33)          
+starting_capital = 60000        
 import_data_from_csv = True     # if using csv exports of yahoo finance data
-save_to_csv = True               # saves a copy of imported yahoo finance data to csv  
-use_bonds = False         # includes ETF proxies for short, medium, long term bonds  
-discrete_shares = True    # display whole number shares after allocation weights
-
-# BLACK LITTERMAN RELATIVE VIEWS
-viewdict = {              # if using BL, need prior weights on each asset to work properly
-    "TLT": 1,             # these weights are based on your belief of expected performance
-    "IWM": -1 
+save_to_csv = True              # saves a copy of imported yahoo finance data to csv  
+discrete_shares = True          # display whole number shares after allocation weights
+optimization_method = 'sharpe'                                                  
+# SHARPE - max return / volatility ratio
+# BLACK - black litterman allows specifiying views on relative asset performance
+viewdict = {    # if using BL, specify manual weights for each included asset to work properly
+    'IWM': -1,
+    'TLT': 0.5,
+    'QQQ': 1
 }
-if (opt == 'black'): 
-    symbols = viewdict.keys()
-
-# MAXIMUM RETURN GIVEN TARGET VOLATILITY
-max_vol = .33             # if maximizing return for target vol
-
-# MINIMIZE VOLATILITY GIVEN TARGET RETURN
-target_return = .33       # if minimizing vol for a target return
-
+# TARGET VOL - max return given a target volatility
+target_vol = .33                    
+# TARGET RETURN min volatlity given a target return
+target_return = .33             
 ## end config ##
 
+# constants
 DATE = datetime.now().strftime("%Y-%m-%d_%H%M%S")
 CWD = os.getcwd() +'/'
 PATH = CWD
-if len(folder) > 0:
-    PATH += folder +'/'
+if len(folder) > 0: PATH += folder +'/'
 TODAY = datetime.today()
-startDate = (TODAY + relativedelta(months=-round(yrs*12))).strftime('%Y-%m-%d')
-endDate = TODAY.strftime('%Y-%m-%d')
-if not use_bonds:
-    ignored_symbols += ['SHY', 'IEF', 'TLT'] 
+START_DATE = (TODAY + relativedelta(months=-round(time_period_in_yrs*12))).strftime('%Y-%m-%d')
+END_DATE = TODAY.strftime('%Y-%m-%d')
 
 # get ticker symbols 
 if import_symbols_from_csv and len(input_file) > 0:
-    symbols = []
     with open(CWD + input_file) as file:
         for line in file:
             name = line.rstrip()
             if name not in ignored_symbols:
                 symbols.append(name)
+    symbols = list(set(symbols))
 
 # Read in price data
 df = pd.DataFrame()
@@ -78,7 +67,7 @@ for sym in symbols:
     if import_data_from_csv:
         df_sym = pd.read_csv(PATH + '{}.csv'.format(sym), parse_dates=True, index_col="Date")
     else:
-        df_sym = pdr.get_data_yahoo(sym, start=startDate, end=endDate)
+        df_sym = pdr.get_data_yahoo(sym, start=START_DATE, end=END_DATE)
         if save_to_csv:
             df_sym.to_csv(PATH + '{}.csv'.format(sym))
 
@@ -90,33 +79,34 @@ for sym in symbols:
     else:
         df = df.join(df_sym, how='outer')
 
+# calculate optimal weights
 mu = mean_historical_return(df)
 cov_matrix = CovarianceShrinkage(df).ledoit_wolf()
 ef = EfficientFrontier(mu, cov_matrix, weight_bounds)
-print ('\nStart Date:', startDate)
-print ('End Date:', endDate)
-print ('Investment Horizon: {} YR'.format(yrs))
+print ('\nStart Date:', START_DATE)
+print ('End Date:', END_DATE)
+print ('Investment Horizon: {} YR'.format(time_period_in_yrs))
 
-if opt == 'sharpe':
+if optimization_method == 'sharpe':
     weights = ef.max_sharpe()
-elif opt == 'min vol':
+elif optimization_method == 'min vol':
     weights = ef.min_volatility()
-elif opt == 'target vol':
-    weights = ef.efficient_risk(max_vol)
-elif opt == 'target return':
+elif optimization_method == 'target vol':
+    weights = ef.efficient_risk(target_vol)
+elif optimization_method == 'target return':
     weights = ef.efficient_return(target_return)
-elif opt == 'black':
+elif optimization_method == 'black':
     bl = BlackLittermanModel(cov_matrix, absolute_views=viewdict)    
-    spx = pdr.get_data_yahoo('SPY', start=startDate, end=endDate)
+    spx = pdr.get_data_yahoo('SPY', start=START_DATE, end=END_DATE)
     spx.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
     delta = black_litterman.market_implied_risk_aversion(spx['Adj Close'])
     bl.bl_weights(delta)
     weights = bl.clean_weights()
     bl.portfolio_performance(verbose=True)
-
-if opt != 'black':
+if optimization_method != 'black':
     ef.portfolio_performance(verbose=True)
 
+# output
 print('\nportfolio allocation weights: ')
 clean_weights = ef.clean_weights()
 for sym, weight in clean_weights.items():
@@ -126,8 +116,8 @@ for sym, weight in clean_weights.items():
 # discrete share allocation
 if discrete_shares:
     latest_prices = get_latest_prices(df)
-    da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=capital)
+    da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=starting_capital)
     allocation, leftover = da.lp_portfolio()
-    print('\ndiscrete share allocation given $', capital)
+    print('\ndiscrete share allocation given $', starting_capital)
     for sym in sorted(allocation):
         print(sym, '\t', allocation[sym])
