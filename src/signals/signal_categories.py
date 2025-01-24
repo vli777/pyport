ac1f2032b59370399e305ba3c313518d59a4ccb4
vintg <10236284+vintg@users.noreply.github.com>
2025-01-24 02:05:13 -0500

    feat(signals): define categories

---
+++ src/signals/signal_categories.py
@@ -0,0 +1,13 @@
+bullish_signals = [
+    "stoch_buy",
+    "stoch_buy_persistence",
+    "bullish_crossover",
+    "preemptive_bullish",
+    "adx_support",
+]
+
+bearish_signals = [
+    "stoch_sell",
+    "bearish_crossover",
+    "preemptive_bearish",
+]
